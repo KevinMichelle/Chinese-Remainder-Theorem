c4c815252f74b238a767279d1b3228ca5086d0b8
Kevin Michelle Contreras González <michelleglz93@gmail.com>
2014-09-18 08:08:25 -0500

    Create chinese.py

---
+++ chinese.py
@@ -0,0 +1,33 @@
+def egcd(a, b):
+	auxiliar = (a, b)
+	a = max(auxiliar)
+	b = min(auxiliar)
+	x = (1, 0)
+	y = (0, 1)
+	while a % b != 0:
+		q = a / b
+		c = a % b
+		nx = x[0] - (x[1] * q)
+		ny = y[0] - (y[1] * q)
+		x = (x[1], nx)
+		y = (y[1], ny)
+		a = b
+		b = c
+	resultado = (b, x[1], y[1])
+	return resultado
+
+def chinese():
+	a = [4, 5, 6, 7, 8, 9]
+	m = [2, 3, 5, 7, 11, 13]
+	n = 1
+	x = 0
+	for i in m:
+		n = n * i
+	for i in xrange(0, len(m)):
+		temporal = m[i]
+		exgcd = egcd(temporal, n / temporal)
+		e = exgcd[1] * (n / temporal)
+		x += e * a[i]
+	print x
+	
+chinese()
